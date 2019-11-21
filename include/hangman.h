a4e53608e5481cc92150a6ae489093dec0466dd9
Quentin <QDM@localhost.localdomain>
2019-11-21 10:46:28 +0100

    [ADD] include: adding the project's library and headers

---
+++ include/hangman.h
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2018
+** personal
+** File description:
+** hangman headers
+*/
+
+#ifndef HANGMAN_H_
+#define HANGMAN_H_
+
+struct data {
+    char *answer;
+    char *word;
+    int tries;
+    char *history;
+};
+typedef struct data data;
+
+typedef enum {
+    HIDDEN = '_',
+    QUIT = -1,
+    LOSE = 0,
+    WIN = 1,
+    CONTINUE = 2,
+} return_values_hangman_t;
+
+void freer(data *, int);
+data *mallocator(int);
+
+#endif
